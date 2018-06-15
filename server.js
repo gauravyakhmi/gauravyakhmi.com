8e57711809dbc909ddbd288a759bec8d5d28a7e1
Gaurav Yakhmi <gaurav.yakhmi@servicenow.com>
2018-06-15 10:10:07 -0700

    uploading working site

--- server.js
+++ server.js
@@ -21,29 +21,29 @@ app.use(require('./routes/api'));
 app.use(require('./routes/chat'));
 
 var PORT = process.env.PORT || 3000;
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
-);
+// app.get('/', (req, res) =>
+//     res.send(`Node and express server is running on port ${PORT}`)
+// );
 
-// var server = app.listen(app.get('port'), function() {
-//   console.log('Listening on port ' + app.get('port'));
-// });
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    console.log(app.get('port'));
-    res.end('new Hello, world!');
-
-}).listen(process.env.PORT || 3000);
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     console.log(app.get('port'));
+//     res.end('new Hello, world!');
+//
+// }).listen(process.env.PORT || 3000);
 
 //console.log('Listening on port: Gaurav ');
 
 
-// io.attach(server);
-// io.on('connection', function(socket) {
-//   socket.on('postMessage', function(data) {
-//     io.emit('updateMessages', data);
-//   });
-// });
-//
-// reload(server, app);
+io.attach(server);
+io.on('connection', function(socket) {
+  socket.on('postMessage', function(data) {
+    io.emit('updateMessages', data);
+  });
+});
+
+reload(server, app);
